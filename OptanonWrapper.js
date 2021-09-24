07be2a023862f7ff987153873795fa1e10e9cc6d
danielanderssonberlin <mail@kenorecords.com>
2021-09-24 12:54:34 +0200

    Update OptanonWrapper.js

--- OptanonWrapper.js
+++ OptanonWrapper.js
@@ -6,11 +6,12 @@ function OptanonWrapper() {
     }
     if (!document.getElementById('ot-confirm-my-choices')) {
         var newNode = document.createElement("BUTTON");
-        newNode.setAttribute('aria-label', "Confirm my choices");
+        var buttonText = document.getElementsByClassName("save-preference-btn-handler")[0].textContent;
+        newNode.setAttribute('aria-label', buttonText);
         newNode.setAttribute('id', "ot-confirm-my-choices");
         newNode.setAttribute('class', "save-preference-btn-handler onetrust-close-btn-handler button-theme");
         newNode.setAttribute('tabindex', 0);
-        newNode.innerHTML = "Confirm Selection";
+        newNode.innerHTML = buttonText;
         newNode.setAttribute('onclick', "OneTrust.Close()");
         var referenceNode = document.querySelector('.ot-sdk-column');
         referenceNode.insertBefore(newNode, referenceNode.nextSibling);
